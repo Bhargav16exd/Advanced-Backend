422f52fcd350604915f2b3e71964c33f9a52cd8d
bhargav16exd <bhargavmule16@gmail.com>
2023-12-19 22:39:15 +0530

    added user model and encrpytion and tokens

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -4,13 +4,13 @@ const asyncHandler = (requestHandler) => {
         Promise.resolve(requestHandler(req,res,next)).
         catch((err)=>next(err))
     }
-    
+
 }
 
-
-
-
 export {asyncHandler}
+
+
+
 
 
 // const asyncHandler = (fn) => {  async (req,res,next) => {
